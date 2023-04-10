3c481f09ac745624f8bf70491f9c62a03a6959ff
Fangzhen Song <songfangzhen@bytedance.com>
2023-04-10 11:37:37 +0800

    WebXRShell: Update *.aar
    This CL contains the following modifications:
    - Update *.aar and MainActivity to match the latest changes.

--- app/src/main/java/com/bytedance/pico/webxrshell/MainActivity.java
+++ app/src/main/java/com/bytedance/pico/webxrshell/MainActivity.java
@@ -26,64 +26,63 @@ import java.util.Collections;
 import java.util.Objects;
 
 public class MainActivity extends WebXRActivity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+  }
 
-    @Override
-    protected void onInitWebXR(@NonNull WebView webView) {
-        WebView.setWebContentsDebuggingEnabled(true);
-        WebViewSettingUtils.setUsualSettings(Objects.requireNonNull(webView));
-        webView.setWebViewClient(new WebViewClient() {
-            @Override
-            public WebResourceResponse shouldInterceptRequest(
-                    WebView view, WebResourceRequest request) {
-                String scheme = request.getUrl().getScheme();
-                if (!"https".equals(scheme) && !"http".equals(scheme)
-                        || !"webxr-samples.local".equals(request.getUrl().getHost())) {
-                    return null;
-                }
-                try {
-                    String path = request.getUrl().getPath();
-                    String mimeType = getMimeType(request.getUrl().toString());
-                    if (!TextUtils.isEmpty(path)) {
-                        String pathInAssets = "webxr-samples" + path;
-                        return new WebResourceResponse(mimeType, "", 200, "ok",
-                                Collections.emptyMap(), getAssets().open(pathInAssets));
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                return null;
-            }
-        });
-        webView.loadUrl("https://webxr-samples.local/tests/navigation/immersive-vr-session.html");
-    }
+  @Override
+  public void onInitWebXR(@NonNull WebView webView) {
+    WebView.setWebContentsDebuggingEnabled(true);
+    WebViewSettingUtils.setUsualSettings(Objects.requireNonNull(webView));
+    webView.setWebViewClient(new WebViewClient() {
+      @Override
+      public WebResourceResponse shouldInterceptRequest(
+          WebView view, WebResourceRequest request) {
+        String scheme = request.getUrl().getScheme();
+        if (!"https".equals(scheme) && !"http".equals(scheme) ||
+            !"webxr-samples.local".equals(request.getUrl().getHost())) {
+          return null;
+        }
+        try {
+          String path = request.getUrl().getPath();
+          String mimeType = getMimeType(request.getUrl().toString());
+          if (!TextUtils.isEmpty(path)) {
+            String pathInAssets = "webxr-samples" + path;
+            return new WebResourceResponse(mimeType, "", 200, "ok",
+                                           Collections.emptyMap(),
+                                           getAssets().open(pathInAssets));
+          }
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+        return null;
+      }
+    });
+    webView.loadUrl(
+        "https://webxr-samples.local/tests/navigation/immersive-vr-session.html");
+  }
 
-    @Override
-    protected Bitmap[] loadWebXRLoadingBitmaps() {
-        LoadingInfo envInfo = new LoadingInfo();
-        envInfo.setContext(this);
-        envInfo.setSplashIconId(R.drawable.webxr_shell);
-        envInfo.setSplashTitle("WebXRShell");
-        envInfo.setEnableBackButton(false);
-        LoadingPicMaker lp = new LoadingPicMaker(envInfo);
-        return lp.generateLoadingBitmaps();
-    }
+  @Override
+  public Bitmap[] loadWebXRLoadingBitmaps() {
+    LoadingInfo envInfo = new LoadingInfo();
+    envInfo.setContext(this);
+    envInfo.setSplashIconId(R.drawable.webxr_shell);
+    envInfo.setSplashTitle("WebXRShell");
+    envInfo.setEnableBackButton(false);
+    LoadingPicMaker lp = new LoadingPicMaker(envInfo);
+    return lp.generateLoadingBitmaps();
+  }
 
-    @Override
-    protected boolean useCustomLoading() {
-        return true;
-    }
+  @Override
+  public boolean enableExperimentalNavigation() {
+    return true;
+  }
 
-    @Override
-    protected boolean enableExperimentalNavigation() { return true; }
-
-    @Override
-    protected XRPosition onLayout() {
-        return new XRPosition(2160, 2160, 0, -0.2f, -0.5f, 1, 1, 1);
-    }
+  @Override
+  public XRPosition onLayout() {
+    return new XRPosition(2160, 2160, 0, -0.2f, -0.5f, 1, 1, 1);
+  }
 
     private static String getMimeType(@Nullable String url) {
         String type = null;
